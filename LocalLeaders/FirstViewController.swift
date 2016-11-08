3727e28e52a55c64be03c0acde6c6fcb0e582cc1
Vince Kearney <vincent.kearney@saltdna.com>
2016-11-08 13:38:58 +0000

    Initial Commit

---
+++ LocalLeaders/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LocalLeaders
+//
+//  Created by Vince Kearney on 08/11/2016.
+//  Copyright © 2016 MAV. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LocalLeaders/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LocalLeaders
+//
+//  Created by Vince Kearney on 08/11/2016.
+//  Copyright © 2016 MAV. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
