bfd5571233ed137bbcbdfd8ab2b663fef6411014
Vince Kearney <vincent.kearney@saltdna.com>
2016-11-16 20:14:02 +0000

    Adding a TabBarController class

---
+++ LocalLeaders/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  LocalLeaders
+//
+//  Created by Vince Kearney on 16/11/2016.
+//  Copyright © 2016 MAV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController : UITabBarController
+{
+    
+}
